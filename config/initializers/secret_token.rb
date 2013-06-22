e3672c0852c39c345e5972f46485b78b4968a20c
EndaMcCormack <endam.ccormack@digital.cabinet-office.gov.uk>
2013-06-22 13:29:40 +0100

    added the basic project files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthHackDublin::Application.config.secret_token = 'adf11397cf6096125f0eeaf8868411a63987a743938c9d58ae919a5820d1a9eba539c3f23d60ec4e70822bf7db079b48185168b30a7feac8c560700c4f1fbc8b'
